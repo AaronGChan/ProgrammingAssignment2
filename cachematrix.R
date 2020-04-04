c5f49838decd0d2fb6c270187291fc8e0befcbc6
aarongchan <achn357@gmail.com>
2020-04-03 21:57:51 -0700

    First push

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,20 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  print("makecachematrix start")
+  i <- NULL
+  set <- function(y){
+    print("Setting")
+    print(y)
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  print("makecachematrix finish")
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+  #print("makecachematrix finish")
 }
 
 
@@ -12,4 +25,21 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  print("Cache start")
+  i <- x$getinverse()
+  if(!is.null(i)){
+    print('got it from memory')
+    return(i)
+  }
+  print("getting")
+  data <- x$get()
+  i <- solve(data)
+  x$setinverse(i)
+  print("Cache finish")
+  i
+  
 }
+f <- rbind(c(1, -1/4), c(-1/4, 1))  
+t <- makeCacheMatrix(f)
+print(cacheSolve(t))
+print(cacheSolve(t))
